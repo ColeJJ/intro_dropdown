6fa4fd3e91a7ca6ac73414f925782a6413c73f1c
ColeJJ <torben.unland@gmail.com>
2022-05-03 21:45:46 +0200

    added body component with some html and comments

---
+++ components/body.tsx
@@ -0,0 +1,19 @@
+/**
+ * 1. Style h1 and p
+ *
+ */
+
+const Body = () => {
+    return (
+        <div>
+            <h1>Make</h1>
+            <h1>remote work</h1>
+            <p>
+                Get your team in sync, no matter your location. Streamline your processes, create
+                team rituals, and watch productivity soar.
+            </p>
+        </div>
+    )
+}
+
+export default Body
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/header'
+import Body from '../components/body'
 
 const Home: NextPage = () => {
     return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
             </Head>
             <body>
                 <Header></Header>
+                <Body></Body>
             </body>
         </div>
     )
