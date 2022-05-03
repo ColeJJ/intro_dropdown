683106fca72d40bb667b0e32a1baebb969983bfb
ColeJJ <torben.unland@gmail.com>
2022-05-03 21:45:13 +0200

    switched to use svg as icon down instead of FontAwesomeIcon

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+
+/**
+ * 1. position icons arrows (next to Features and Company)
+ * 2. position snap logo into center
+ */
 
 const Header = () => {
     return (
@@ -13,10 +19,16 @@ const Header = () => {
                 {/* nav */}
                 <div className="hidden items-center space-x-8 md:flex">
                     <h3>
-                        Features <FontAwesomeIcon icon={faAngleDown} />
+                        Features{' '}
+                        <Image
+                            className="flex items-center"
+                            src="/images/icon-arrow-down.svg"
+                            width={10}
+                            height={6}
+                        />
                     </h3>
                     <h3>
-                        Company <FontAwesomeIcon icon={faAngleDown} />
+                        Company <Image src="/images/icon-arrow-down.svg" width={10} height={6} />
                     </h3>
                     <h3>Careers</h3>
                     <h3>About</h3>
