3b83fe660cc7f4e8c3df9337bbdbe4999e6877a7
ColeJJ <torben.unland@gmail.com>
2022-07-06 10:09:57 +0200

    renamed body to text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/header'
-import Body from '../components/body'
+import Text from '../components/text'
 
 /**
  * 1. remove FontAwesomeIcon
@@ -9,18 +9,18 @@ import Body from '../components/body'
  */
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <body>
-                <Header></Header>
-                <Body></Body>
-            </body>
-        </div>
-    )
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head >
+			<body>
+				<Header></Header>
+				<Text></Text>
+			</body>
+		</div >
+	)
 }
 
 export default Home
