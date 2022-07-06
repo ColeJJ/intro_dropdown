b61359ee8d1a30e50727d3213752ca749b173691
ColeJJ <torben.unland@gmail.com>
2022-07-06 10:09:23 +0200

    added learn more button, fixed positions

---
+++ components/text.tsx
@@ -0,0 +1,20 @@
+/**
+ * 1. Style h1 and p
+ *
+ */
+
+const Text = () => {
+	return (
+		<div className="relative h-full top-28 left-32">
+			<h1 className="text-8xl font-bold">Make</h1>
+			<h1 className="text-8xl font-bold">remote work</h1>
+			<p className="relative top-12 max-w-lg">
+				Get your team in sync, no matter your location. Streamline your processes, create
+				team rituals, and watch productivity soar.
+				<button className="relative top-9 rounded-2xl border-2 px-6 py-2 border-black bg-black text-white">Learn more</button>
+			</p>
+		</div>
+	)
+}
+
+export default Text 
