0a8cb3949b04867db541ee26acb4a71c7b437508
ColeJJ <torben.unland@gmail.com>
2022-07-13 10:45:07 +0200

    added picture to the side

---
+++ components/bodypic.tsx
@@ -0,0 +1,12 @@
+const BodyPic = () => {
+    return (
+        <div>
+            <img
+                src="./images/image-hero-desktop.png"
+                className="absolute top-1/3 right-56 w-4/12"
+            />
+        </div>
+    )
+}
+
+export default BodyPic
