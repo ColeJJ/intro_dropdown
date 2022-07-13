5f8b313550d21fe87fefd5c0cc6d31c2227c3441
ColeJJ <torben.unland@gmail.com>
2022-07-13 10:46:14 +0200

    made databiz logo a bit bigger

--- components/partners.tsx
+++ components/partners.tsx
@@ -1,7 +1,7 @@
 const Partners = () => {
 	return (
 		<div className='relative flex space-x-12 top-80 left-32'>
-			<img src='../images/client-databiz.svg' className='w-24 h-5' />
+			<img src='../images/client-databiz.svg' className='w-28 h-5' />
 			<img src='../images/client-audiophile.svg' className='' />
 			<img src='../images/client-meet.svg' className='h-6' />
 			<img src='../images/client-maker.svg' className='h-6' />
