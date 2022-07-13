b541b9eaed82509b741e75b1b36b5571677aee39
ColeJJ <torben.unland@gmail.com>
2022-07-13 10:45:47 +0200

    pushed text component lower

--- components/text.tsx
+++ components/text.tsx
@@ -1,15 +1,17 @@
 const Text = () => {
-	return (
-		<div className="relative h-full top-28 left-32">
-			<h1 className="text-8xl font-bold">Make</h1>
-			<h1 className="text-8xl font-bold">remote work</h1>
-			<p className="relative top-12 max-w-lg">
-				Get your team in sync, no matter your location. Streamline your processes, create
-				team rituals, and watch productivity soar.
-				<button className="relative top-9 rounded-2xl border-2 px-6 py-2 border-black bg-black text-white">Learn more</button>
-			</p>
-		</div>
-	)
+    return (
+        <div className="relative top-48 left-32 h-full">
+            <h1 className="text-8xl font-bold">Make</h1>
+            <h1 className="text-8xl font-bold">remote work</h1>
+            <p className="relative top-12 max-w-lg">
+                Get your team in sync, no matter your location. Streamline your processes, create
+                team rituals, and watch productivity soar.
+                <button className="relative top-9 rounded-2xl border-2 border-black bg-black px-6 py-2 text-white">
+                    Learn more
+                </button>
+            </p>
+        </div>
+    )
 }
 
-export default Text 
+export default Text
